f61b00d8d1ea520cefad074b420d08f271cad2d3
Subhpreet kaur <subhpreet.kaur@dcmail.ca>
2026-01-21 18:00:09 -0500

    Fix routes and add profile API

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hello from WSL + VS Code 🎉");
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("🚀 Hello Subhpreet! Your Express server is running in WSL.");
+});
+
+app.get("/about", (req, res) => {
+  res.send("This is my first Node + Express app 🎉");
+});
+
+app.get("/api/profile", (req, res) => {
+  res.json({
+    name: "Subhpreet",
+    program: "Computer Programming",
+    tools: ["WSL", "VS Code", "Node", "Express"],
+    status: "Building my portfolio 🚀"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
